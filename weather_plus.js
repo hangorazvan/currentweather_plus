6ce757b8d7d269797f7fc2f3813c42d8bc34ae60
Horia Razvan Cristea <58172000+hangorazvan@users.noreply.github.com>
2021-04-15 16:01:50 +0300

    Add files via upload

--- weather_plus.js
+++ weather_plus.js
@@ -293,8 +293,7 @@ Module.register("weather_plus",{
 			small.className = "normal medium rfd ";
 
 			var feelsLike = document.createElement("span");
-					if (this.config.units == "metric") {
-				if (this.feelsLike == -0) {this.feelsLike = 0}
+						if (this.config.units == "metric") {
 				if (this.feelsLike >= 45) {
 					feelsLike.className = "real redrf";
 				} else if (this.feelsLike >= 40 && this.feelsLike < 45) {
